6d85de9b96cb5725da6bbbf93df63b096e693cbd
jhonathanalencar <jhonathan.alencarfs@gmail.com>
2022-09-21 14:25:02 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,145 @@
+export function App() {
+  return (
+    <>
+      <header>
+        <img src="" alt="logo" />
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <div className="hero">
+          <h1>We are creatives</h1>
+        </div>
+        <div className="about">
+          <section>
+            <div>
+              <h2>Transform your brand</h2>
+              <p>
+                We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
+              </p>
+              <a href="#">
+                <strong>Learn more</strong>
+              </a>
+            </div>
+            <div>
+              <img src="" alt="image" />
+            </div>
+          </section>
+          <section>
+            <div>
+              <img src="" alt="image" />
+            </div>
+            <div>
+              <h2>Stand out to the right audience</h2>
+              <p>
+                Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.
+              </p>
+              <a href="#">
+                <strong>Learn more</strong>
+              </a>
+            </div>
+          </section>
+        </div>
+        <div className="services">
+          <div>
+            <h3>Graphic Design</h3>
+            <p>
+              Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients attention.
+            </p>
+          </div>
+          <div>
+            <h3>Photography</h3>
+            <p>
+              Increase your credibility by getting the most stunning, high-quality photos that improve your business image.
+            </p>
+          </div>
+        </div>
+        <div className="testimonials">
+          <strong>Client testimonials</strong>
+          <article>
+            <img src="" alt="picture" />
+            <blockquote>
+              <p>
+                We put our trust in Sunnyside and they delivered, making sure out needs were met and deadlines were always hit.
+              </p>
+              <cite>
+                  <strong>Emily R.</strong>
+                  <span>Marketing Director</span>
+              </cite>
+            </blockquote> 
+          </article>
+          <article>
+            <img src="" alt="picture" />
+            <blockquote>
+              <p>
+                Sunnyside's enthusiasm coupled with their keen interest in our brand's success made it a satisfying and enjoyable experience.
+              </p>
+              <cite>
+                  <strong>Thomas S.</strong>
+                  <span>Chief Operating Officer</span>
+              </cite>
+            </blockquote> 
+          </article>
+          <article>
+            <img src="" alt="picture" />
+            <blockquote>
+              <p>
+                Incredible end result! Our sales increased over 400% when we worked with Sunnyside. Highly recommended!
+              </p>
+              <cite>
+                  <strong>Jennie F.</strong>
+                  <span>Business Owner</span>
+              </cite>
+            </blockquote> 
+          </article>
+        </div>
+        <div className="projects">
+          <img src="" alt="project" />
+          <img src="" alt="project" />
+          <img src="" alt="project" />
+          <img src="" alt="project" />
+        </div>
+      </main>
+      <footer>
+        <strong>sunnyside</strong>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">About</a>
+              <a href="#">Services</a>
+              <a href="#">Projects</a>
+            </li>
+          </ul>
+        </nav>
+        <ul>
+          <li>
+            <a href="#">facebook</a>
+          </li>
+          <li>
+            <a href="#">instagram</a>
+          </li>
+          <li>
+            <a href="#">twitter</a>
+          </li>
+          <li>
+            <a href="#">pinterest</a>
+          </li>
+        </ul>
+      </footer>
+    </>
+  )
+}
